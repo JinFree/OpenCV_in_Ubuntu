63f0766d7a5ab11a9a903661d435dda66cab9504
JinFree <ytrqwe12@gmail.com>
2019-08-01 16:52:28 +0900

    add trafficlight path

--- 2day_course/C++/OpenCV_Functions.cpp
+++ 2day_course/C++/OpenCV_Functions.cpp
@@ -4,6 +4,27 @@ void imageProcessing(Mat &image, Mat &result)
     result = imageCopy(image);
     return;
 }
+string path_to_images(void)
+{   
+    string road_image_01 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidWhiteCurve.jpg";
+    string road_image_02 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidWhiteRight.jpg";
+    string road_image_03 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidYellowCurve.jpg";
+    string road_image_04 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidYellowCurve2.jpg";
+    string road_image_05 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidYellowLeft.jpg";
+    string road_image_06 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/whiteCarLaneSwitch.jpg";
+    string road_image_07 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/test.png";
+
+    string traffic_image_01 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_01.png";
+    string traffic_image_02 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_02.png";
+    string traffic_image_03 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/red_light_01.png";
+    string traffic_image_04 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/red_light_02.png";
+    string traffic_image_05 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/yellow_light_01.png";
+    string traffic_image_06 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/yellow_light_02.png";
+    string traffic_image_07 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/traffic_light_image_01.PNG";
+    string traffic_image_08 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/traffic_light_image_02.PNG";
+    string traffic_image_09 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/traffic_light_image_03.PNG";
+    return traffic_image_01;
+}
 void processingSingleImage(string imagePath, string outputPath)
 {
     Mat image = imageRead(imagePath);
@@ -15,11 +36,12 @@ void processingSingleImage(string imagePath, string outputPath)
     imageWrite(outputPath, result);
     return;
 }
-vector<string> imageList(void)
+vector<string> imageList_LaneDetection(void)
 {
     string path_to_project = "/home/opencv-mds/OpenCV_in_Ubuntu/";
     string path_to_data = path_to_project + "Data/";
     string path_to_road_image = path_to_data + "Lane_Detection_Images/";
+
     string road_image_01 = path_to_road_image + "solidWhiteCurve.jpg";
     string road_image_02 = path_to_road_image + "solidWhiteRight.jpg";
     string road_image_03 = path_to_road_image + "solidYellowCurve.jpg";
@@ -38,11 +60,38 @@ vector<string> imageList(void)
     images.push_back(road_image_07);
     return images;
 }
-void processingMultipleImages(void)
+vector<string> imageList_TrafficLightDetection(void)
 {
-    vector<string> images = imageList();
+    string path_to_project = "/home/opencv-mds/OpenCV_in_Ubuntu/";
+    string path_to_data = path_to_project + "Data/";
+    string path_to_road_image = path_to_data + "TrafficLight_Detection/";
+
+    string image_01 = path_to_road_image + "green_light_01.png";
+    string image_02 = path_to_road_image + "green_light_02.png";
+    string image_03 = path_to_road_image + "red_light_01.png";
+    string image_04 = path_to_road_image + "red_light_02.png";
+    string image_05 = path_to_road_image + "yellow_light_01.png";
+    string image_06 = path_to_road_image + "yellow_light_02.png";
+    string image_07 = path_to_road_image + "traffic_light_image_01.PNG";
+    string image_08 = path_to_road_image + "traffic_light_image_02.PNG";
+    string image_09 = path_to_road_image + "traffic_light_image_03.PNG";
+
+    vector<string> images;
+    images.push_back(image_01);
+    images.push_back(image_02);
+    images.push_back(image_03);
+    images.push_back(image_04);
+    images.push_back(image_05);
+    images.push_back(image_06);
+    images.push_back(image_07);
+    images.push_back(image_08);
+    images.push_back(image_09);
+    return images;
+}
+void processingMultipleImages(vector<string> list_of_images)
+{
     int idx=0;
-    for (string path : images)
+    for (string path : list_of_images)
     {
         Mat image = imageRead(path);
         imageShow("Image index is " + to_string(idx), image);
@@ -54,12 +103,19 @@ void processingMultipleImages(void)
     }
     return;
 }
+string path_to_videos(void)
+{
+    string road_video_01 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/solidWhiteRight.mp4";
+    string road_video_02 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/solidYellowLeft.mp4";
+    string road_video_03 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/challenge.mp4";
+    return road_video_01;
+}
 void processingSingleVideo(string videoPath, string outputPath)
 {
     Video(videoPath, outputPath);
     return;
 }
-vector<string> videoList(void)
+vector<string> videoList_LaneDetection(void)
 {
     string path_to_project = "/home/opencv-mds/OpenCV_in_Ubuntu/";
     string path_to_data = path_to_project + "Data/";
@@ -74,11 +130,10 @@ vector<string> videoList(void)
     videos.push_back(road_video_03);
     return videos;
 }
-void processingMultipleVideos(void)
+void processingMultipleVideos(vector<string> list_of_videos)
 {
-    vector<string> videos = videoList();
     int idx=0;
-    for (string path : videos)
+    for (string path : list_of_videos)
     {
         Video(path, "output_" + to_string(idx) + ".mp4");
         idx++;
--- 2day_course/Python/OpenCV_Functions.py
+++ 2day_course/Python/OpenCV_Functions.py
@@ -6,6 +6,27 @@ def imageProcessing(image):
     return result
 
 
+def path_to_images():
+    road_image_01 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidWhiteCurve.jpg"
+    road_image_02 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidWhiteRight.jpg"
+    road_image_03 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidYellowCurve.jpg"
+    road_image_04 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidYellowCurve2.jpg"
+    road_image_05 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidYellowLeft.jpg"
+    road_image_06 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/whiteCarLaneSwitch.jpg"
+    road_image_07 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/test.png"
+
+    traffic_image_01 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_01.png"
+    traffic_image_02 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_02.png"
+    traffic_image_03 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/red_light_01.png"
+    traffic_image_04 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/red_light_02.png"
+    traffic_image_05 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/yellow_light_01.png"
+    traffic_image_06 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/yellow_light_02.png"
+    traffic_image_07 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/traffic_light_image_01.PNG"
+    traffic_image_08 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/traffic_light_image_02.PNG"
+    traffic_image_09 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/traffic_light_image_03.PNG"
+    return traffic_image_01
+
+
 def processingSingleImage(imagePath, outputPath = "output.jpg"):
     image = imageRead(imagePath)
     imageShow("Opened Image", image)
@@ -15,32 +36,59 @@ def processingSingleImage(imagePath, outputPath = "output.jpg"):
     return
 
 
-def imageList():
+def imageList_LaneDetection():
     path_to_project = "/home/opencv-mds/OpenCV_in_Ubuntu/"
     path_to_data = path_to_project + "Data/"
     path_to_road_image = path_to_data + "Lane_Detection_Images/"
 
-    road_image_01 = path_to_road_image + "solidWhiteCurve.jpg"
-    road_image_02 = path_to_road_image + "solidWhiteRight.jpg"
-    road_image_03 = path_to_road_image + "solidYellowCurve.jpg"
-    road_image_04 = path_to_road_image + "solidYellowCurve2.jpg"
-    road_image_05 = path_to_road_image + "solidYellowLeft.jpg"
-    road_image_06 = path_to_road_image + "whiteCarLaneSwitch.jpg"
-    road_image_07 = path_to_road_image + "test.png"
+    image_01 = path_to_road_image + "solidWhiteCurve.jpg"
+    image_02 = path_to_road_image + "solidWhiteRight.jpg"
+    image_03 = path_to_road_image + "solidYellowCurve.jpg"
+    image_04 = path_to_road_image + "solidYellowCurve2.jpg"
+    image_05 = path_to_road_image + "solidYellowLeft.jpg"
+    image_06 = path_to_road_image + "whiteCarLaneSwitch.jpg"
+    image_07 = path_to_road_image + "test.png"
 
     images=[]
-    images.append(road_image_01)
-    images.append(road_image_02)
-    images.append(road_image_03)
-    images.append(road_image_04)
-    images.append(road_image_05)
-    images.append(road_image_06)
-    images.append(road_image_07)
+    images.append(image_01)
+    images.append(image_02)
+    images.append(image_03)
+    images.append(image_04)
+    images.append(image_05)
+    images.append(image_06)
+    images.append(image_07)
     return images
 
 
-def processingMultipleImages():
-    list_of_images = imageList()
+def imageList_TrafficLightDetection():
+    path_to_project = "/home/opencv-mds/OpenCV_in_Ubuntu/"
+    path_to_data = path_to_project + "Data/"
+    path_to_road_image = path_to_data + "TrafficLight_Detection/"
+
+    image_01 = path_to_road_image + "green_light_01.png"
+    image_02 = path_to_road_image + "green_light_02.png"
+    image_03 = path_to_road_image + "red_light_01.png"
+    image_04 = path_to_road_image + "red_light_02.png"
+    image_05 = path_to_road_image + "yellow_light_01.png"
+    image_06 = path_to_road_image + "yellow_light_02.png"
+    image_07 = path_to_road_image + "traffic_light_image_01.PNG"
+    image_08 = path_to_road_image + "traffic_light_image_02.PNG"
+    image_09 = path_to_road_image + "traffic_light_image_03.PNG"
+
+    images=[]
+    images.append(image_01)
+    images.append(image_02)
+    images.append(image_03)
+    images.append(image_04)
+    images.append(image_05)
+    images.append(image_06)
+    images.append(image_07)
+    images.append(image_08)
+    images.append(image_09)
+    return images
+
+
+def processingMultipleImages(list_of_images):
     for index in range(len(list_of_images)):
         image = imageRead(list_of_images[index])
         imageShow("Image index is {}".format(index), image)
@@ -50,12 +98,19 @@ def processingMultipleImages():
     return
 
 
+def path_to_videos():
+    road_video_01 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/solidWhiteRight.mp4"
+    road_video_02 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/solidYellowLeft.mp4"
+    road_video_03 = "/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/challenge.mp4"
+    return road_video_01
+
+
 def processingSingleVideo(videoPath, outputPath = "output.avi"):
     Video(videoPath, outputPath)
     return
 
 
-def videoList():
+def videoList_LaneDetection():
     path_to_project = "/home/opencv-mds/OpenCV_in_Ubuntu/"
     path_to_data = path_to_project + "Data/"
     path_to_road_video = path_to_data + "Lane_Detection_Videos/"
@@ -70,8 +125,7 @@ def videoList():
     return videos
 
 
-def processingMultipleVideos():
-    list_of_videos = videoList()
+def processingMultipleVideos(list_of_videos):
     for index in range(len(list_of_videos)):
         Video(list_of_videos[index], "output_{}.mp4".format(index))
     return
