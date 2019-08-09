04538f9f2874ef13a792a162836ffa2ef2474a78
Jin Yeong Wang <ytrqwe12@gmail.com>
2019-08-09 12:38:48 +0900

    Update OpenCV_Utils.py

--- 4day_course/Python/OpenCV_Utils.py
+++ 4day_course/Python/OpenCV_Utils.py
@@ -484,12 +484,14 @@ def lineFitting(image, lines, color = (0,0,255), thickness = 3, slope_threshold 
     right = medianPoint(rights)
     min_y = int(height*0.6)
     max_y = height
-    min_x_left = interpolate(left[1], left[2], left[3], left[4], min_y)
-    max_x_left = interpolate(left[1], left[2], left[3], left[4], max_y)
-    min_x_right = interpolate(right[1], right[2], right[3], right[4], min_y)
-    max_x_right = interpolate(right[1], right[2], right[3], right[4], max_y)
-    cv2.line(result, (min_x_left, min_y), (max_x_left, max_y), color, thickness)
-    cv2.line(result, (min_x_right, min_y), (max_x_right, max_y), color, thickness)
+    if left is not None:
+        min_x_left = interpolate(left[1], left[2], left[3], left[4], min_y)
+        max_x_left = interpolate(left[1], left[2], left[3], left[4], max_y)
+        cv2.line(result, (min_x_left, min_y), (max_x_left, max_y), color, thickness)
+    if right is not None:
+        min_x_right = interpolate(right[1], right[2], right[3], right[4], min_y)
+        max_x_right = interpolate(right[1], right[2], right[3], right[4], max_y)
+        cv2.line(result, (min_x_right, min_y), (max_x_right, max_y), color, thickness)
     return result
 
 
