df7cb6cf0a284de281e6d2c5e6137a0cd7c62c98
JinFree <ytrqwe12@gmail.com>
2019-12-27 08:53:53 +0900

    edit path

--- 2day_course/C++/OpenCV_Main.cpp
+++ 2day_course/C++/OpenCV_Main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main(void) 
 {
     // Traffic Light Detection Project
-    //processingSingleImage("/home/opencv-mds/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_01.png");
+    //processingSingleImage("/home/opencv/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_01.png");
     //processingMultipleImages(imageList_TrafficLightDetection());
 
     // Lane Detection Project
-    //processingSingleImage("/home/opencv-mds/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/test.png");
-    processingMultipleImages(imageList_LaneDetection());
-    //processingSingleVideo(path_to_videos());
+    //processingSingleImage("/home/opencv/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/test.png");
+    //processingMultipleImages(imageList_LaneDetection());
+    processingSingleVideo(path_to_videos());
     //processingMultipleVideos(videoList_LaneDetection());
     return 0;
 }
