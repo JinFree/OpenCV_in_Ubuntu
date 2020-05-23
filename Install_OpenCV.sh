c63338f45e4f7776e057c8b8ec5fb74402cb6037
Jin Yeong Wang <ytrqwe12@gmail.com>
2020-05-23 12:49:36 +0900

    Update Install_OpenCV.sh

--- Install_OpenCV.sh
+++ Install_OpenCV.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 sudo add-apt-repository ppa:jonathonf/ffmpeg-4
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
-sudo apt-get install -y apt-utils cmake unzip gedit vim curl pkg-config build-essential cmake libjpeg-dev libgflags-dev libtiff5-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev libv4l-dev ffmpeg v4l-utils gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev libqt4-dev mesa-utils libgl1-mesa-dri libqt4-opengl-dev libatlas-base-dev gfortran libeigen3-dev python2.7-dev python3-dev python-numpy python3-numpy python-pip python3-pip
+sudo apt-get install -y \
+apt-utils cmake unzip gedit vim curl pkg-config build-essential \
+gstreamer1.0-tools gstreamer1.0-alsa \
+ gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+ gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
+ gstreamer1.0-libav \
+ libgstreamer1.0-dev \
+ libgstreamer-plugins-base1.0-dev \
+ libgstreamer-plugins-good1.0-dev \
+ libgstreamer-plugins-bad1.0-dev \
+ cmake libgflags-dev v4l-utils pyqt5-dev-tools\
+ libjpeg-dev libgflags-dev libtiff5-dev libpng-dev libavcodec-dev libavformat-dev \
+ libswscale-dev libxvidcore-dev libx264-dev libxine2-dev \
+ libv4l-dev ffmpeg v4l-utils gstreamer1.0-tools gstreamer1.0-alsa \
+ gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+ gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer1.0-dev \
+ libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev \
+ libgstreamer-plugins-bad1.0-dev libqt4-dev mesa-utils libgl1-mesa-dri \
+ libqt4-opengl-dev libatlas-base-dev gfortran libeigen3-dev python2.7-dev \
+ python3-dev python-numpy python3-numpy python-pip python3-pip
 cd && mkdir OPENCV_INSTALL && cd OPENCV_INSTALL && wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.0.zip && unzip opencv.zip && wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.0.zip && unzip opencv_contrib.zip && rm opencv.zip && rm opencv_contrib.zip
 cd && cd OPENCV_INSTALL/opencv-3.4.0/ && mkdir build && cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
