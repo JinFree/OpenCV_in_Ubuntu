0193d844730e8c7bbb187162f0b9f6655e5b10eb
Jin Yeong Wang <ytrqwe12@gmail.com>
2021-03-16 14:45:04 +0900

    Update Install_OpenCV.sh

--- Install_OpenCV.sh
+++ Install_OpenCV.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo add-apt-repository ppa:jonathonf/ffmpeg-4
 sudo apt-get update
+sudo apt install gcc make cmake curl vim git python-dev python3-dev \
+ python-pip python3-pip libgflags-dev v4l-utils pyqt5-dev-tools build-essential \
+ python3-dev cython3 python3-setuptools python3-pip python3-wheel python3-numpy \
+ python3-pytest python3-blosc python3-brotli python3-snappy python3-lz4 zlib1g-dev \
+ libblosc-dev liblzma-dev liblz4-dev libzstd-dev libpng-dev libwebp-dev libbz2-dev \
+ libopenjp2-7-dev libjpeg-turbo8-dev libjxr-dev liblcms2-dev libcharls-dev libaec-dev \
+ libbrotli-dev libsnappy-dev libzopfli-dev libgif-dev libtiff-dev -y
 sudo apt-get install -y \
 apt-utils cmake unzip curl pkg-config build-essential \
 gstreamer1.0-tools gstreamer1.0-alsa \
