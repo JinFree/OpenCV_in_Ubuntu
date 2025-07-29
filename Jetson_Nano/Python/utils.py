7c63d611b7d40a200725154e9e62d91132ef208f
Jinyeong Wang <ytrqwe12@gmail.com>
2025-07-29 13:36:24 +0900

    Update utils.py
    for python3 in jetson

--- Jetson_Nano/Python/utils.py
+++ Jetson_Nano/Python/utils.py
@@ -2,6 +2,6 @@ import numpy as np
 import cv2
 from opencv_functions import *
 
-def process(image_src: cv2.Mat, image_name: str) -> None:
+def process(image_src, image_name):
     return image_src
     
